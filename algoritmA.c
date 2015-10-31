0ca2bb670c5e9060f063c118a00b0a5ec294bc20
Shinsy <shinsy1023@gmail.com>
2015-11-01 06:02:34 +0900

    start

---
+++ algoritmA.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+  int junbeom=0;
+  int yunseong=0;
+  int j_day=0;
+  int y_day=0;
+  for (i=0;i<7;i++){
+    scanf("%d %d",&junbeom, &yunseong);
+    
+  return 0;
+}
