2f43ecb7ba103f1acc060c78bc373317983b0dcc
Shinsy <shinsy1023@gmail.com>
2015-11-01 06:12:41 +0900

    error

--- algoritmA.c
+++ algoritmA.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 
 int main(){
-  int junbeom=0;
-  int yunseong=0;
+  int jun=0;
+  int yun=0;
   int j_day=0;
   int y_day=0;
-  for (i=0;i<7;i++){
-    scanf("%d %d",&junbeom, &yunseong);
-    
+  for (int i=0;i<7;i++){
+    j_day=0;
+    y_day=0;
+    scanf("%d %d",&j_day, &y_day);
+    jun=jun+j_day;
+    yun=yun+y_day;
+  }
+  if (jun>yun){
+    printf("God junbeom bless you");
+  }else if (yun>jun){
+    printf("Cute Yunseong I love you");
+  }else{
+    printf("Seo A Jeong solo");
+  }
   return 0;
 }
