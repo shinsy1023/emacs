a2f00d6c220194b0374e11b962169be31bcf20ff
Shinsy <shinsy1023@gmail.com>
2015-11-01 06:18:55 +0900

    How to pass?

--- algoritmA.c
+++ algoritmA.c
@@ -11,13 +11,14 @@ int main(){
     scanf("%d %d",&j_day, &y_day);
     jun=jun+j_day;
     yun=yun+y_day;
+    printf("%d %d\n",jun,yun);
   }
   if (jun>yun){
-    printf("God junbeom bless you");
+    printf("God junbeom bless you\n");
   }else if (yun>jun){
-    printf("Cute Yunseong I love you");
+    printf("Cute Yunseong I love you\n");
   }else{
-    printf("Seo A Jeong solo");
+    printf("Seo A Jeong solo\n");
   }
   return 0;
 }
