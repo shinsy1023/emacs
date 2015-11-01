6e5f75663bf6547412d7f1157f1a6253957b32cb
Shinsy <shinsy1023@gmail.com>
2015-11-01 12:24:35 +0900

    what's this?

--- algorithmB.c
+++ algorithmB.c
@@ -3,31 +3,32 @@
 
 int main(){
   int count=0;
-  int* height=(int*)malloc(sizeof(int));
+  int* height;
   long long** pascal;
   int i, j, k;
   scanf("%d",&count);
-  pascal=(long long**)malloc(sizeof(long long*));
+  height=(int*)malloc(sizeof(int)*count);
   for (i=0;i<count;i++){
     scanf("%d",&height[i]);
   }
-  printf("%lld",pascal[0][0]);
   for (i=0;i<count;i++){
+    pascal=(long long**)malloc(sizeof(long long**)*height[i]);
     for(j=0;j<height[i];j++){
-      pascal[j]=(long long*)malloc(sizeof(long long)*j);
+      pascal[j]=(long long*)malloc(sizeof(long long*)*j);
       pascal[j][0]=1;
       pascal[j][j]=1;
-      printf("good\n");
+      if(j!=0){
       printf("%lld ",pascal[j][0]);
+      }
       if (j>0){
-  	for (k=1;k<j+1;k++){
-  	  pascal[j][k]=pascal[j][k-1]+pascal[j-1][k];
+  	for (k=1;k<j;k++){
+  	  pascal[j][k]=pascal[j-1][k-1]+pascal[j-1][k];
   	  printf("%lld ",pascal[j][k]);
   	}
       }
-      printf("%lld",pascal[j][j]);
+      printf("%lld\n",pascal[j][j]);
     }
-    pascal=NULL;
+    free(pascal);
   }
   return 0;
 }
