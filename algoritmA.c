860658434100fb5fffb66d608a67fc07127da9d4
Shinsy <shinsy1023@gmail.com>
2015-11-01 11:26:00 +0900

    perfect

--- algoritmA.c
+++ algoritmA.c
@@ -5,20 +5,26 @@ int main(){
   int yun=0;
   int j_day=0;
   int y_day=0;
-  for (int i=0;i<7;i++){
-    j_day=0;
-    y_day=0;
-    scanf("%d %d",&j_day, &y_day);
-    jun=jun+j_day;
-    yun=yun+y_day;
-    printf("%d %d\n",jun,yun);
-  }
-  if (jun>yun){
-    printf("God junbeom bless you\n");
-  }else if (yun>jun){
-    printf("Cute Yunseong I love you\n");
-  }else{
-    printf("Seo A Jeong solo\n");
+  int count=0;
+  scanf("%d",&count);
+  for (int j=0;j<count;j++){
+    for (int i=0;i<7;i++){
+      j_day=0;
+      y_day=0;
+      scanf("%d %d",&j_day, &y_day);
+      jun=jun+j_day;
+      yun=yun+y_day;
+      printf("%d %d\n",jun,yun);
+    }
+    if (jun>yun){
+      printf("God junbeom bless you\n");
+    }else if (yun>jun){
+      printf("Cute Yunseong I love you\n");
+    }else{
+      printf("Seo A Jeong solo\n");
+    }
+    jun=0;
+    yun=0;
   }
   return 0;
 }
