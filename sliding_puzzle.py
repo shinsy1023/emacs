44623a26ef2c040b84566651569e0a688236d791
shinsy <shinsy1023@gmail.com>
2015-11-05 14:41:50 +0900

    3.0 points

--- sliding_puzzle.py
+++ sliding_puzzle.py
@@ -4,11 +4,10 @@ def sliding_puzzle():
 	goal=[[1, 2, 3, 4],
 	[5, 6, 7, 8],
 	[9, 10, 11, 12],
-	[13, 14, 15, '']]
+	[13, 14, 15, 0]]
 	(board, empty)=create_init_board()
 	while True:
 		print_board(board)
-		print("Type the number you want to move (Type 0 to quit): ")
 		if board==goal:
 			printf("Congratulations!")
 			break
@@ -24,31 +23,57 @@ def print_board(board):
 	for i in range (4):
 		print(str(i)+"|",end='')
 		for j in range(4):
-			print(str(board[i][j]).rjust(2),end=' ')
-		print()
+			if board[i][j]!=0:
+				if j!=3:
+					print(str(board[i][j]).rjust(2),end=' ')
+				else:
+					print(str(board[i][j]).rjust(2))
+			else:
+				if j!=3:
+					print(' '.rjust(2),end=' ')
+				else:
+					print(' '.rjust(2))
 
-def trans(board):
-	board1=[[],[],[],[]]
+def create_init_board():
+	a=0
+	b=3
+	board1=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0]
+	board=[[],[],[],[]]
+	random.shuffle(board1)
+	for i in range(4):	
+		for j in range(4):
+			board[i].append(board1[a])
+			a+=1
 	for i in range(4):
 		for j in range(4):
-			board1[i].append(board[j][i])
-			print(board1)
-	return board1
-def create_init_board():
-	a=3
-	b=0
-	board=[[1, 2, 3, 4],[5, 6, 7, 8],[9, 10, 11, 12],[13, 14, 15, 0]]
-	for i in range(2):	
-		for i in range(4):
-			random.shuffle(board[i])
-			a=(board[3].index(0))
-		board=trans(board)
-		b=(board[a].index(0))
-	board=trans(board)
-	print(b,a, board)
-	return (board,(b,a))
+			if board[i][j]==0:
+				a=i
+				b=j
+				break
+	return (board,(a,b))
 
-#def get_number():
-        
+def get_number():
+	num=input("Type the number you want to move (Type 0 to quit): ")
+	while not(num.isdigit() and 0<=int (num)<=15):
+		num=input("Type the number you want to move (Type 0 to quit): ")
+	return int(num)
 
-sliding_puzzle()
+def find_position(num, board):
+	for i in range (4):
+		for j in range(4):
+			if board[i][j]==int(num):
+				return (i, j)
+
+def move(pos, opened, board):
+	if pos[0]==opened[0]:
+		if pos[1]==opened[1]+1 or pos[1]==opened[1]-1:
+			board[opened[0]][opened[1]], board[pos[0]][pos[1]]\
+			=board[pos[0]][pos[1]], board[opened[0]][opened[1]]
+			return (pos, board)
+	if pos[1]==opened[1]:
+		if pos[0]==opened[0]+1 or pos[0]==opened[0]-1:
+			board[opened[0]][opened[1]], board[pos[0]][pos[1]]\
+			=board[pos[0]][pos[1]], board[opened[0]][opened[1]]
+			return (pos, board)
+	print("Can't move! Try again!")
+	return (opened, board)
