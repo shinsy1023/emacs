2e76e3efdc0a9667f27b9e4d7e4e9165fede36b7
Shinsy <shinsy1023@gmail.com>
2015-11-08 17:56:51 +0900

    perfect

---
+++ algorithmI.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int compare(const void *a, const void *b){
+  if(*(int*)a<*(int*)b){
+    return -1;
+  }else if (*(int*)a ==*(int*)b){
+    return 0;
+  }
+  return 1;
+}
+
+int main(){
+  int count, i, j;
+  int length[500]={0};
+  int array[500]={0};
+  int price=0;
+  scanf("%d", &count);
+  for (i=0;i<count;i++){
+    scanf("%d", &length[i]);
+  }
+  for (i=0; i<count; i++){
+    for (j=0; j<length[i]; j++){
+      scanf("%d", &array[j]);
+    }
+    for (j=0; j<length[i]-1;j++){
+      qsort(array, length[i], sizeof(int),compare);
+      printf("%d %d\n", array[j], array[j+1]);
+      price=array[j]+array[j+1]+price;
+      array[j+1]=array[j+1]+array[j];
+      array[j]=0;
+    }
+    printf("%d\n", price);
+    price=0;
+  }
+  return 0;
+}
